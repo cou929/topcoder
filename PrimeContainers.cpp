14a48d0ee6d671778f8fdc6e4f52b25872a9b7a7
Kosei Moriyama <cou929@gmail.com>
2010-09-06 22:27:47 +0900

    test

--- PrimeContainers.cpp
+++ PrimeContainers.cpp
@@ -1,5 +1,3 @@
-// BEGIN CUT HERE
-// END CUT HERE
 #include <sstream>
 #include <string>
 #include <vector>
@@ -15,9 +13,6 @@
 #include <queue>
 #include <stack>
 #include <numeric>
-// BEGIN CUT HERE
-#include "cout.hpp"
-// END CUT HERE 
 using namespace std;
 
   bool isPrime(int n) {
@@ -50,55 +45,10 @@ public:
     return ret;
   }
    
-  // BEGIN CUT HERE
-public:
-	void run_test(int Case) { 
-    if ((Case == -1) || (Case == 0)) test_case_0();
-    if ((Case == -1) || (Case == 1)) test_case_1();
-    if ((Case == -1) || (Case == 2)) test_case_2();
-    if ((Case == -1) || (Case == 3)) test_case_3();
-    if ((Case == -1) || (Case == 4)) test_case_4();
-    if ((Case == -1) || (Case == 5)) test_case_5();
-    if ((Case == -1) || (Case == 6)) test_case_6();
-  }
-private:
-	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
-	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
-	void test_case_0() { 
-    int Arg0 = 10; 
-    int Arg1 = 2; 
-    verify_case(0, Arg1, containerSize(Arg0)); }
-	void test_case_1() { 
-    int Arg0 = 42; 
-    int Arg1 = 2; 
-    verify_case(1, Arg1, containerSize(Arg0)); }
-	void test_case_2() { 
-    int Arg0 = 47; 
-    int Arg1 = 5; 
-    verify_case(2, Arg1, containerSize(Arg0)); }
-	void test_case_3() { 
-    int Arg0 = 959; 
-    int Arg1 = 6; 
-    verify_case(3, Arg1, containerSize(Arg0)); }
-	void test_case_4() { 
-    int Arg0 = 421337; 
-    int Arg1 = 2; 
-    verify_case(4, Arg1, containerSize(Arg0)); }
-	void test_case_5() { 
-    int Arg0 = 10; 
-    int Arg1 = 2; 
-    verify_case(5, Arg1, containerSize(Arg0)); }
-	void test_case_6() { 
-    int Arg0 = 10; 
-    int Arg1 = 2; 
-    verify_case(6, Arg1, containerSize(Arg0)); }
-
-  // END CUT HERE
 
 };
-// BEGIN CUT HERE
-int main() {
-PrimeContainers ___test;
-___test.run_test(-1);
-}
-// END CUT HERE 
+
+
+// Powered by FileEdit
+// Powered by TZTester 1.01 [25-Feb-2003]
+// Powered by CodeProcessor
