16f27a17e7d6e7632336707d25527e1b8410e733
Kosei Moriyama <cou929@gmail.com>
2011-04-20 22:12:49 +0900

    add problem 018

---
+++ project_euler/018.py
@@ -0,0 +1,19 @@
+N = int(raw_input())
+
+tree = []
+dp = [[0 for i in xrange(N)] for j in xrange(N)]
+
+for i in xrange(N):
+    tmp = map(int, raw_input().split())
+    for i in xrange(N - len(tmp)):
+        tmp.append(0)
+    tree.insert(0, tmp)
+
+for row in xrange(N):
+    for col in xrange(N - row):
+        if row > 0:
+            dp[row][col] = tree[row][col] + max(dp[row-1][col], dp[row-1][col+1])
+        elif row == 0:
+            dp[row][col] = tree[row][col]
+
+print dp[N-1][0]
