9bdeca59f097a60f8a6a1300266b5d8bbf826d0c
Kosei Moriyama <cou929@gmail.com>
2011-04-26 23:48:33 +0900

    solve problem 024

--- project_euler/024.py
+++ project_euler/024.py
@@ -3,4 +3,19 @@ def fact(n):
 
 facts = [fact(i) for i in range(1, 10 + 1)]
 
-print facts
+def find(n, vec):
+    if n == 0:
+        vec.sort()
+        vec.reverse()
+        return reduce(lambda x, y: str(x) + str(y), vec)
+
+    f = facts[len(vec) - 2]
+    index = int(n / f)
+    if n % f == 0:
+        tmp = vec.pop(index - 1)
+    else:
+        tmp = vec.pop(index)
+
+    return str(tmp) + str(find(n - f * index, vec))
+
+print find(1000000, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
